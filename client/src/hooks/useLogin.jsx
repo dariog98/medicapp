98d83878088c993fb2de7c0c9439c746d6fc6546
dariog98 <dariogoirad98@gmail.com>
2024-01-11 15:19:14 -0300

    router and login updated

--- client/src/hooks/useLogin.jsx
+++ client/src/hooks/useLogin.jsx
@@ -6,23 +6,60 @@ import { useUserContext } from '../components/providers/UserProvider'
 import { useNavigate } from 'react-router-dom'
 import { ROUTES } from '../constants/routes'
 import userServices from '../services/userServices'
+import { useSettingsContext } from '../components/providers/SettingsProvider'
+import { useNotificationsContext } from '../components/providers/NotificationsProvider'
+import { TOAST_TIME } from '../constants/time'
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
 
 const useLogin = () => {
-    const form = useForm({ resolver: yupResolver(schemaLogin) })
-    const [isLoading, setIsLoading] = useState(false)
-    const { handleLogIn } = useUserContext()
     const navigate = useNavigate()
+    const { language } = useSettingsContext()
+    const { addNotification } = useNotificationsContext()
+    const { handleLogIn } = useUserContext()
+    const [isLoading, setIsLoading] = useState(false)
+    const form = useForm({ resolver: yupResolver(schemaLogin) })
+
+    const ACTIONS = {
+        200: (data) => {
+            handleLogIn(data)
+            navigate(ROUTES.Home)
+        },
+        404: () => {
+            addNotification({
+                id: Date.now(),
+                icon: faTriangleExclamation,
+                message: language.messages.UserNotFound,
+                type: 'warning',
+                time: TOAST_TIME.Short
+            })
+        },
+        409: () => {
+            addNotification({
+                id: Date.now(),
+                icon: faTriangleExclamation,
+                message: language.messages.InvalidPassword,
+                type: 'warning',
+                time: TOAST_TIME.Short
+            })
+        },
+        default: () => {
+            addNotification({
+                id: Date.now(),
+                icon: faTriangleExclamation,
+                message: language.messages.AnErrorOcurred,
+                type: 'warning',
+                time: TOAST_TIME.Short
+            })
+        }
+    }
 
     const handleConfirm = async (data) => {
         try {
             setIsLoading(true)
             const response = await userServices.login(data)
-            if (response.status === 200) {
-                handleLogIn(response.data)
-                navigate(ROUTES.Home)
-            }
+            ACTIONS[response.status](response.data)
         } catch (error) {
-            console.log(error)
+            ACTIONS.default()
         } finally {
             setIsLoading(false)
         }
